501464ff38f43ff40e53c3b0791ebf4bb0a97515
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-05-14 10:27:03 +0300

    Search filter

--- src/Table.vue
+++ src/Table.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <router-link tag="button" class="btn btn-primary" :to='"/add"'>Create new phone</router-link>
+    <router-link tag="button" class="btn btn-primary" :to="'/add'"
+      >Create new phone</router-link
+    >
+    <input type="text" v-model="search" placeholder="Search title.." />
+    <select name="search_filter" v-model="selected">
+      <option value="name" v-bind:value="'name'">Name</option>
+      <option value="model" v-bind:value="'model'">Model</option>
+      <option value="price" v-bind:value="'price'">Price</option>
+    </select>
 
     <div class="col-6 offset-3">
       <table class="table table-hover">
@@ -11,14 +19,18 @@
           <th>price</th>
           <th></th>
         </tr>
-        <tr v-for="item in phones" :key="item.id">
-          <td>{{item.id}}</td>
-          <td>{{item.name}}</td>
-          <td>{{item.model}}</td>
-          <td>{{item.price}}</td>
+        <tr v-for="item in filteredList" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.model }}</td>
+          <td>{{ item.price }}</td>
           <td>
-            <button class="btn-sm btn-warning" @click='editPhone(item.id)'>Edit</button>
-            <button class="btn-sm btn-danger" @click="deletePhone(item.id)">Delete</button>
+            <button class="btn-sm btn-warning" @click="editPhone(item.id)">
+              Edit
+            </button>
+            <button class="btn-sm btn-danger" @click="deletePhone(item.id)">
+              Delete
+            </button>
           </td>
         </tr>
       </table>
@@ -31,6 +43,8 @@ export default {
   data() {
     return {
       url: "http://localhost:3000/phones",
+      selected: "name",
+      search: "",
       phones: []
     };
   },
@@ -54,15 +68,31 @@ export default {
     },
     // Перенаправлення на маршрут редагування
     editPhone(id) {
-      this.$router.push('/edit/'+id);
+      this.$router.push("/edit/" + id);
     }
   },
   // Ініціалізація списку автомобілів при створенні компонента
   created() {
     this.getPhones();
+  },
+  computed: {
+    filteredList() {
+      if (this.selected == "name") {
+        return this.phones.filter(post => {
+          return post.name.toLowerCase().includes(this.search.toLowerCase());
+        });
+      } else if (this.selected == "model") {
+        return this.phones.filter(post => {
+          return post.model.toLowerCase().includes(this.search.toLowerCase());
+        });
+      } else if (this.selected == "price") {
+        return this.phones.filter(post => {
+          return post.price.toLowerCase().includes(this.search.toLowerCase());
+        });
+      }
+    }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
